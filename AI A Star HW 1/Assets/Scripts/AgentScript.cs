f60b7aa579dc7ace283fd076c3d64ac0f3ee88e4
Brock Soicher <brocksoicher@msn.com>
2017-04-09 22:38:47 -0600

    A* HW1

---
+++ AI A Star HW 1/Assets/Scripts/AgentScript.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentScript : MonoBehaviour {
+	// Control variables - movement speed and movement method
+	[Range(0.01f, 1.0f)]
+	public float speed = 0.15f;
+	public bool useVelocity = true;
+
+	// Control variable - draw search debug info
+	public bool showDebug = false;
+
+	// Controls the object used to indicate our next target
+	public GameObject target;
+	// Gives access to map for pathfinding purposes
+	public Map map;
+	// Heuristic for searching
+	Octile octile;
+
+	// Used for searching
+	GridAStar astar;
+
+	// List of waypoints to follow
+	List<xyLoc> waypoints;
+
+	// Current and goal location
+	xyLoc currentLoc;
+	xyLoc goal;
+
+	// Control for managing timeslicing of search
+	bool doingSearch = false;
+
+	// Use this for initialization
+	void Start () {
+		octile = new Octile ();
+		waypoints = new List<xyLoc>();
+
+		// Sample set of waypoints. Movement is from last to first
+		waypoints.Add (new xyLoc ( 0,  0));
+		waypoints.Add (new xyLoc (49,  0));
+		waypoints.Add (new xyLoc (49, 49));
+		waypoints.Add (new xyLoc ( 0, 49));
+		astar = new GridAStar(50, 50);
+		useVelocity = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 1. Timeslice search if it is ongoing. One expansion for frame
+		//    In practice you would do more.
+		if (doingSearch) {
+			if (astar.Step (currentLoc, goal, map)) {
+				doingSearch = false;
+				waypoints = astar.ExtractPath ();
+			}
+		}
+		if (showDebug)
+			astar.DebugDraw ();
+
+		// Left mouse is down in this frame
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, 100)) {
+				target.transform.position = new Vector3(Mathf.RoundToInt(hit.point.x), 0.5f, Mathf.RoundToInt(hit.point.z));
+				goal = new xyLoc (Mathf.RoundToInt(hit.point.x), Mathf.RoundToInt(hit.point.z));
+
+				// Initialize incremental search
+				if (showDebug) {
+					astar.Init (currentLoc, goal, map, octile);
+					doingSearch = true;
+				} else {
+					// perform full search immediately
+					waypoints = astar.GetPath (currentLoc, goal, map, octile);
+				}
+			}
+		}
+
+		// Don't do movement if we are searching
+		if (doingSearch)
+			return;
+
+		// Reached current waypoint
+		if ((waypoints.Count > 0) &&
+			(waypoints[waypoints.Count-1].SquaredDistance (transform.position.x, transform.position.z) < 0.01)) {
+			waypoints.RemoveAt (waypoints.Count-1);
+		}
+
+		// Draw upcoming path
+		if (waypoints.Count > 0)
+			Debug.DrawLine (transform.position, new Vector3 (waypoints [waypoints.Count-1].x, 1.0f, waypoints [waypoints.Count-1].y), Color.red);
+		for (int x = 0; x < waypoints.Count-1; x++) {
+			Debug.DrawLine (new Vector3 (waypoints [x].x, 1.0f, waypoints [x].y), new Vector3 (waypoints [x + 1].x, 1.0f, waypoints [x + 1].y), Color.red);
+		}
+
+		// Move to next waypoint
+		if (waypoints.Count > 0) {
+			// Set target location
+			target.transform.position = new Vector3 (waypoints [waypoints.Count-1].x, target.transform.position.y, waypoints [waypoints.Count-1].y);
+
+			// Update our location
+			if (useVelocity) {
+				Vector3 diff = new Vector3 (waypoints [waypoints.Count-1].x, transform.position.y, waypoints [waypoints.Count-1].y);
+				diff -= transform.position;
+				if (diff.magnitude > speed) {
+					diff.Normalize ();
+					diff *= speed;
+				}
+				transform.position += diff;
+			} else {
+				transform.position = new Vector3 (transform.position.x * 0.9f + waypoints [waypoints.Count-1].x * 0.1f,
+					transform.position.y,
+					transform.position.z * 0.9f + waypoints [waypoints.Count-1].y * 0.1f);
+			}
+			currentLoc = new xyLoc (Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z));
+		}
+	}
+}
---
+++ AI A Star HW 1/Assets/Scripts/GridAStar.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GridAStar {
+
+	/*
+	 * openItem: Private data structure with items needed inside the open/closed list.
+	 */
+	struct openItem {
+		public float gCost;
+		public float hCost;
+		public float fCost;
+		public xyLoc parent;
+		public int round; // entry is only valid if the stored round is equal to the current round
+		public bool open;
+	};
+
+	// The map dimensions - used for allocating the size of the openClosed list
+	int width, height;
+	openItem[,] openClosed;
+
+	// Keeps track of how many pathfinding calls have been made. 
+	int round;
+
+	// Goal location
+	xyLoc g;
+
+	// Heuristic function
+	Heuristic h;
+
+	// Flag to know if path was found
+	bool success;
+
+	// Initialize data structures and internal variables here
+	public GridAStar(int maxWidth, int maxHeight)
+	{
+        // Write code here.
+        width = maxWidth;
+        height = maxHeight;
+        openClosed = new openItem[maxWidth, maxHeight];
+	}
+
+	// Initialize any variables specific for this search
+	public void Init(xyLoc start, xyLoc goal, Map m, Heuristic heur)
+	{
+		// Write code here.
+        g = goal;
+        h = heur;
+        round = 0;
+        AddToOpen(start);
+       // Debug.Log(openClosed[0, 0].parent.ToString());
+
+	}
+
+    // Continue the previous search. (Assumes Init is called first.)
+    // Returns true when search is complete
+    public bool Step(xyLoc start, xyLoc goal, Map m)
+    {
+        Expand(m, start);
+
+		// Write code here.
+
+		return true;
+	}
+
+	// Find complete path from start to goal using incremental search
+	// functions defined above
+	public List<xyLoc> GetPath(xyLoc start, xyLoc goal, Map m, Heuristic heur)
+	{
+		if (start == goal)
+			return new List<xyLoc>();
+		Init (start, goal, m, heur);
+
+		while (Step(start, goal, m) == false) { Debug.Log("Probably stuck here"); }
+
+		return ExtractPath ();
+	}
+
+	// Add the start state to the open list
+	void AddToOpen(xyLoc s)
+	{
+        Debug.Log("Add first xyLoc");
+		AddToOpen (s, 0f, s);
+	}
+
+    // Add state to open list. If it is alreay in closed, ignore.
+    // If it is already in open, update parent and gCost (if shorter path found)
+    // Otherwise add to open
+    void AddToOpen(xyLoc s, float gCost, xyLoc parent)
+    {
+        // Write code here.
+        if (!openClosed[s.x, s.y].open && openClosed[s.x, s.y].fCost > 0)
+            return;
+
+        openItem item = openClosed[s.x, s.y];
+
+        item.parent = parent;
+
+        item.gCost = openClosed[parent.x, parent.y].gCost + s.SquaredDistance(parent.x, parent.y);
+        item.hCost = h.HCost(s, g);
+
+        float fCost = item.gCost + item.hCost;
+
+        item.round = round;
+
+        if (item.open && item.fCost > fCost)
+        {
+            item.fCost = fCost;
+            item.parent = parent;
+        }
+        else if (!item.open)
+        {
+            item.fCost = fCost;
+        }
+
+        item.open = true;
+
+        openClosed[s.x, s.y] = item;
+        Debug.Log(item.fCost.ToString());
+        Debug.Log(openClosed[s.x, s.y].fCost.ToString());
+    }
+	// Move from open to closed
+	void AddToClosed(xyLoc s)
+	{
+        // Write code here.
+        openClosed[s.x, s.y].open = false;
+	}
+
+	// Look at the map to find the successors of s
+	// (This could have also gone inside the map itself)
+	// Use the map functions to check passability
+	void Expand(Map m, xyLoc s)
+	{
+        // Write code here.
+        xyLoc nextSpot = s;
+
+        for (int i = 0; i < 4; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    nextSpot.y = s.y + 1;
+                    break;
+                case 1:
+                    nextSpot.y = s.y - 1;
+                    break;
+                case 2:
+                    nextSpot.x = s.x + 1;
+                    break;
+                case 3:
+                    nextSpot.x = s.x - 1;
+                    break;
+                default:
+                    break;
+
+            }
+
+            if (!m.IsOccupied(nextSpot.x, nextSpot.y))
+                AddToOpen(nextSpot, nextSpot.SquaredDistance(s.x, s.y), s);
+        }
+
+        Expand(m, GetNextToExpand());
+
+	}
+
+	// Find the next best state to expand
+	xyLoc GetNextToExpand()
+	{
+		xyLoc best = new xyLoc(-1,-1);
+        round++;
+        float fCost = Mathf.Infinity;
+		// Write code here.
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y< height; y++)
+            {
+                if(openClosed[x,y].open && openClosed[x,y].fCost < fCost)
+                {
+                    fCost = openClosed[x, y].fCost;
+                    best.x = x;
+                    best.y = y;
+                    Debug.Log("New best at :" + best.ToString());
+                       
+                }
+
+            }
+        }
+
+        AddToClosed(best);
+		return best;
+	}
+
+	// Extract the path from the given state to the goal.
+	// (Only runs if search successfully found the goal.)
+	public List<xyLoc> ExtractPath()
+	{
+		List<xyLoc> result = new List<xyLoc> ();
+        // Write code here.
+        xyLoc item = new xyLoc(-1, -1);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (!openClosed[x, y].open && openClosed[x,y].parent != null)
+                {
+                    item.x = x;
+                    item.y = y;
+                    result.Add(item);
+                }
+
+            }
+        }
+        return result;
+	}
+
+	public void DebugDraw()
+	{
+		// Write code here.
+
+	
+	}
+}
